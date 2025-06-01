724cd4c4ded9ab852491181482b20de922a6deb4
vasisht <vasishtnirmal2004@gmail.com>
2025-06-01 09:57:35 +0000

    fixed fill method

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while(True):
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        pygame.Surface.fill(screen, (0, 0, 0))
+        screen.fill("black")
         pygame.display.flip()
 
 if __name__ == "__main__":
